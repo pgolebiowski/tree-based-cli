7950a30f74be42118ec49eef8a5c0bdd0d0b7cb1
Patryk Golebiowski <pgolebiowski@users.noreply.github.com>
2022-12-27 15:43:15 +0000

    Improve annotation of `LeafCommand`

--- src/TreeBasedCli/Commands/Leaf/WithArguments/LeafCommand-ArgumentsParserHandler.cs
+++ src/TreeBasedCli/Commands/Leaf/WithArguments/LeafCommand-ArgumentsParserHandler.cs
@@ -6,7 +6,21 @@ using TreeBasedCli.Exceptions;
 namespace TreeBasedCli
 {
     /// <summary>
-    /// Same as <see cref="LeafCommand"/> but powered with dependency injection.
+    /// Represents a leaf command in a command tree. It is designed to be the most flexible and powerful way
+    /// to create leaf commands, and is intended for use in scenarios where you need to specify options and/or
+    /// inject custom dependencies.
+    /// 
+    /// <para>- <typeparamref name="TArguments"/> is a custom class that represents the already-parsed arguments
+    /// to your command options.</para>
+    /// <para>- <typeparamref name="TParser"/> is a custom class that parses user input into an instance
+    /// of <typeparamref name="TArguments"/>.</para>
+    /// <para>- <typeparamref name="THandler"/> is a custom class that handles the execution
+    /// of your command asynchronously.</para>
+    /// 
+    /// To use this class, you will need to derive from it and provide implementations for <typeparamref name="TArguments"/>,
+    /// <typeparamref name="TParser"/>, <typeparamref name="THandler"/> that suit your specific needs. This allows you
+    /// to tailor your leaf commands to the unique requirements of your CLI and create a highly customized
+    /// and user-friendly experience for your users.
     /// </summary>
     public class LeafCommand<TArguments, TParser, THandler> : LeafCommand, ICommand<TArguments, TParser, THandler>
         where TArguments : IParsedCommandArguments
