314b59b63f4d7c031c5f9b382238321eb7177dd5
Patryk Golebiowski <pgolebiowski@users.noreply.github.com>
2023-01-12 00:57:37 +0000

    Annotate `CommandOption`

--- src/TreeBasedCli/Commands/Leaf/WithArguments/CommandOption.cs
+++ src/TreeBasedCli/Commands/Leaf/WithArguments/CommandOption.cs
@@ -1,10 +1,25 @@
 using TreeBasedCli.Exceptions;
-using TreeBasedCli.Extensions;
 
 namespace TreeBasedCli
 {
+    /// <summary>
+    /// Represents a command option, which is a user-specified flag or parameter
+    /// that modifies the behavior of a command.
+    /// </summary>
     public class CommandOption
     {
+        /// <summary>
+        /// Creates a new instance of the <see cref="CommandOption"/> class, which represents
+        /// a user-specified flag or parameter that modifies the behavior of a command.
+        /// </summary>
+        /// <param name="label">
+        /// The string representing the label for the option.
+        /// It cannot be null, empty, or consist exclusively of whitespace characters.
+        /// </param>
+        /// <param name="description">
+        /// A description of the option, its usage and its effects. Each item of the array
+        /// will be generated in documentation as a separate paragraph.
+        /// </param>
         public CommandOption(string label, string[] description)
         {
             if (string.IsNullOrWhiteSpace(label))
@@ -19,7 +34,14 @@ namespace TreeBasedCli
             this.Description = description;
         }
 
+        /// <summary>
+        /// Gets the string representing the label for the option.
+        /// </summary>
         public string Label { get; }
+
+        /// <summary>
+        /// Gets the description of the option, its usage and its effects.
+        /// </summary>
         public string[] Description { get; }
     }
 }
